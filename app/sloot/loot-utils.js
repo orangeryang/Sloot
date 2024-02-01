1891a6f08da0111f4af07e064f4df0032304d7a5
Steif <40876894+Again1013@users.noreply.github.com>
2024-02-01 15:07:34 +0800

    fix

--- app/sloot/loot-utils.js
+++ app/sloot/loot-utils.js
@@ -1,4 +1,6 @@
-// const mergeImages = require("merge-images")
+// https://github.com/stephancill/synthetic-loot-viewer
+
+const mergeImages = require("merge-images")
 const deploymentMap = require("../../public/map.json")
 
 const mapping = require("../../public/item_layer_mapping.json")
@@ -140,90 +142,90 @@ function itemsFromSvg(svg) {
     return items;
 }
 
-var defaultOptions = {
-    format: 'image/png',
-    quality: 0.92,
-    width: undefined,
-    height: undefined,
-    Canvas: undefined,
-    crossOrigin: undefined
-};
-
-var mergeImages = function (sources, options) {
-    if (sources === void 0) sources = [];
-    if (options === void 0) options = {};
-    
-    return new Promise(function (resolve) {
-        options = Object.assign({}, defaultOptions, options);
-        
-        // Setup browser/Node.js specific variables
-        var Canvas = options.Canvas || require('canvas');
-        var canvas = options.Canvas ? new options.Canvas() : new Canvas();
-        var Image = options.Image || Canvas.Image;
-        
-        // Load sources
-        var images = sources.map(function (source) {
-            return new Promise(function (resolve, reject) {
-                // Convert sources to objects
-                if (source.constructor.name !== 'Object') {
-                    source = { src: source };
-                }
-                
-                // Resolve source and img when loaded
-                var img = new Image();
-                img.src = source.src;
-                img.onerror = function () {
-                    reject(new Error("Couldn't load image"));
-                };
-                img.onload = function () {
-                    resolve(Object.assign({}, source, { img: img }));
-                };
-            });
-        });
-        
-        // Get canvas context
-        var ctx = canvas.getContext('2d');
-        
-        // When sources have loaded
-        resolve(
-            Promise.all(images).then(function (images) {
-                // Set canvas dimensions
-                var getSize = function (dim) {
-                    return options[dim] || Math.max.apply(Math, images.map(function (image) {
-                        return image.img[dim];
-                    }));
-                };
-                canvas.width = getSize('width');
-                canvas.height = getSize('height');
-                
-                // Draw images to canvas
-                images.forEach(function (image) {
-                    ctx.globalAlpha = image.opacity ? image.opacity : 1;
-                    ctx.drawImage(image.img, image.x || 0, image.y || 0);
-                });
-                
-                if (options.Canvas && options.format === 'image/jpeg') {
-                    // Resolve data URI for node-canvas jpeg async
-                    return new Promise(function (resolve, reject) {
-                        canvas.toDataURL(options.format, { quality: options.quality, progressive: false }, function (
-                            err,
-                            jpeg
-                        ) {
-                            if (err) {
-                                reject(err);
-                            } else {
-                                resolve(jpeg);
-                            }
-                        });
-                    });
-                }
-                
-                // Resolve all other data URIs sync
-                return canvas.toDataURL(options.format, options.quality);
-            })
-        );
-    });
-};
+// var defaultOptions = {
+//     format: 'image/png',
+//     quality: 0.92,
+//     width: undefined,
+//     height: undefined,
+//     Canvas: undefined,
+//     crossOrigin: undefined
+// };
+//
+// var mergeImages = function (sources, options) {
+//     if (sources === void 0) sources = [];
+//     if (options === void 0) options = {};
+//
+//     return new Promise(function (resolve) {
+//         options = Object.assign({}, defaultOptions, options);
+//
+//         // Setup browser/Node.js specific variables
+//         var Canvas = options.Canvas || require('canvas');
+//         var canvas = options.Canvas ? new options.Canvas() : new Canvas();
+//         var Image = options.Image || Canvas.Image;
+//
+//         // Load sources
+//         var images = sources.map(function (source) {
+//             return new Promise(function (resolve, reject) {
+//                 // Convert sources to objects
+//                 if (source.constructor.name !== 'Object') {
+//                     source = { src: source };
+//                 }
+//
+//                 // Resolve source and img when loaded
+//                 var img = new Image();
+//                 img.src = source.src;
+//                 img.onerror = function () {
+//                     reject(new Error("Couldn't load image"));
+//                 };
+//                 img.onload = function () {
+//                     resolve(Object.assign({}, source, { img: img }));
+//                 };
+//             });
+//         });
+//
+//         // Get canvas context
+//         var ctx = canvas.getContext('2d');
+//
+//         // When sources have loaded
+//         resolve(
+//             Promise.all(images).then(function (images) {
+//                 // Set canvas dimensions
+//                 var getSize = function (dim) {
+//                     return options[dim] || Math.max.apply(Math, images.map(function (image) {
+//                         return image.img[dim];
+//                     }));
+//                 };
+//                 canvas.width = getSize('width');
+//                 canvas.height = getSize('height');
+//
+//                 // Draw images to canvas
+//                 images.forEach(function (image) {
+//                     ctx.globalAlpha = image.opacity ? image.opacity : 1;
+//                     ctx.drawImage(image.img, image.x || 0, image.y || 0);
+//                 });
+//
+//                 if (options.Canvas && options.format === 'image/jpeg') {
+//                     // Resolve data URI for node-canvas jpeg async
+//                     return new Promise(function (resolve, reject) {
+//                         canvas.toDataURL(options.format, { quality: options.quality, progressive: false }, function (
+//                             err,
+//                             jpeg
+//                         ) {
+//                             if (err) {
+//                                 reject(err);
+//                             } else {
+//                                 resolve(jpeg);
+//                             }
+//                         });
+//                     });
+//                 }
+//
+//                 // Resolve all other data URIs sync
+//                 return canvas.toDataURL(options.format, options.quality);
+//             })
+//         );
+//     });
+// };
 
 module.exports = {getImageForLoot, itemsFromSvg}
 
