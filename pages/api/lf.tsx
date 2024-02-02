952577b1263b50a8041f69a528a47a57501f5a38
Steif <40876894+Again1013@users.noreply.github.com>
2024-02-02 16:18:27 +0800

    fix

--- pages/api/lf.tsx
+++ pages/api/lf.tsx
@@ -2,17 +2,17 @@
 import {useRouter} from "next/navigation";
 import {useEffect} from "react";
 
-export default function Handler() {
-    const router = useRouter();
+export default function Home() {
+    // const router = useRouter();
     
-    useEffect(() => {
+    // useEffect(() => {
         // const youtubeUrl = 'https://loot.foundation/';
         
         // Perform the redirect
         // window.location.href = youtubeUrl; // For a full page reload redirect
         // Or use Next.js router for client-side redirect (comment out the line above if using this)
         // router.push(youtubeUrl);
-    }, [router]);
+    // }, [router]);
     
     return (
         <div>
