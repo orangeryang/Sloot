fac2b5e57369709b5ef68105c795f056bb8f5abe
Steif <40876894+Again1013@users.noreply.github.com>
2024-02-02 16:12:56 +0800

    fix

--- pages/api/lf.tsx
+++ pages/api/lf.tsx
@@ -6,10 +6,10 @@ export default function Handler() {
     const router = useRouter();
     
     useEffect(() => {
-        const youtubeUrl = 'https://loot.foundation/';
+        // const youtubeUrl = 'https://loot.foundation/';
         
         // Perform the redirect
-        window.location.href = youtubeUrl; // For a full page reload redirect
+        // window.location.href = youtubeUrl; // For a full page reload redirect
         // Or use Next.js router for client-side redirect (comment out the line above if using this)
         // router.push(youtubeUrl);
     }, [router]);
