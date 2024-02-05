d7d9c61b1930255fbafb9ca955ccff88dcd8cb32
Steif <40876894+Again1013@users.noreply.github.com>
2024-02-05 14:24:10 +0800

    ss

--- pages/api/sloot.tsx
+++ pages/api/sloot.tsx
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             
             const add = req.query["address"];
             
-            const path = "./cache/" + add;
+            const path = "~/lootframe/sloot/cache/" + add;
             console.log("path: ", path);
             readFile(path, "utf8", (err, data) => {
                 if (err) {
