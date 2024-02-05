af5497f43cee2a9e472721a6390bbda5da286cc0
Steif <40876894+Again1013@users.noreply.github.com>
2024-02-05 13:57:52 +0800

    fix

--- pages/api/sloot.tsx
+++ pages/api/sloot.tsx
@@ -1,18 +1,11 @@
+import { Contract, JsonRpcProvider } from 'ethers';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-// import sharp from 'sharp';
-// import satori from "satori";
-// import { ethers, JsonRpcProvider, Contract } from "ethers";
-// import SyntheticLootArtifact from "../../public/SyntheticLoot.json";
-// import map from "../../public/map.json";
-// import { itemsFromSvg, getImageForLoot } from "@/app/sloot/loot-utils";
-
-
-// const IMG_DIR = `ipfs://${map.ipfs.character_imgs}`;
-
-
 import puppeteer from "puppeteer";
+import satori from "satori";
+import sharp from "sharp";
 // import chromium from 'chrome-aws-lambda';
+import { readFile, writeFile } from "fs";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
@@ -21,11 +14,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             
             const add = req.query["address"];
             
+            const path = "./cache/" + add;
+            console.log("path: ", path);
+            readFile(path, "utf8", (err, data) => {
+                if (err) {
+                    console.error(err);
+                }
+                if (data) {
+                    res.setHeader('Content-Type', 'image/png');
+                    res.setHeader('Cache-Control', 'max-age=10');
+                    res.send(Buffer.from(data, 'base64'));
+                }
+            })
+            
             const browser = await puppeteer.launch({
                 args: ['--window-size=1910,1000'],
             })
             
-            // const browser = await puppeteer.launch(/*{headless: false}*/{args: ['--window-size=1910,1000']});
             const page = await browser.newPage();
             await page.goto("https://loot.stephancill.co.za/#/address/" + add, {waitUntil: "load"});
             await page.waitForSelector("#root > div > div:nth-child(4) > div.TokenCard_tokenCard__2xW3d > div > div:nth-child(2) > div > div > div.Token_token__2gp1y > img", {timeout: 50000});
@@ -41,39 +46,47 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             //     {width: 1910, height: 1000, fonts: []}
             // );
             
-            await page.setViewport({width: 1910, height: 1000});
-            await page.goto(img || "about:blank", {waitUntil: "load"});
             
-            const snap = await page.screenshot();
-            
-            // const sloot = new Contract("0x869Ad3Dfb0F9ACB9094BA85228008981BE6DBddE", ["function tokenURI(address) public view returns (string)",], new JsonRpcProvider("https://rpc.mevblocker.io"));
-            // // console.log("sloot:", sloot);
-            // const tokenURIB64 = await sloot.tokenURI(/*address[0]*/);
-            // // console.log("tokenUTIB64", tokenURIB64);
-            // const tokenURI = JSON.parse(Buffer.from(tokenURIB64.split(",")[1], 'base64').toString("utf8"))
-            // // console.log("tokenURI:", tokenURI);
-            // const b64svg = tokenURI.image;
-            // // console.log("b64svg:", b64svg);
+            const sloot = new Contract("0x869Ad3Dfb0F9ACB9094BA85228008981BE6DBddE", ["function tokenURI(address) public view returns (string)",], new JsonRpcProvider("https://rpc.mevblocker.io"));
+            // console.log("sloot:", sloot);
+            const tokenURIB64 = await sloot.tokenURI(add);
+            // console.log("tokenUTIB64", tokenURIB64);
+            const tokenURI = JSON.parse(Buffer.from(tokenURIB64.split(",")[1], 'base64').toString("utf8"))
+            // console.log("tokenURI:", tokenURI);
+            const b64svg = tokenURI.image;
+            // console.log("b64svg:", b64svg);
             // const svg = Buffer.from(b64svg.split(",")[1], 'base64').toString("utf8")
-            // // console.log("svg:", svg);
-            //
+            // console.log("svg:", svg);
+            
+            
             // const items = itemsFromSvg(svg)
             // console.log("items:", items)
             // const img = await getImageForLoot(items)
             // console.log("img:", img)
             
-            //
-            // const satoriSvg = await satori(
-            //     <div className="card" style={{backgroundColor: "white", marginTop: "15px"}}>
-            //         <img alt="character" style={{borderRadius: "5px", width: "100%"}} src={img}/>
-            //         <ul style={{marginLeft: "-20px"}}>{items.map(item => {
-            //             return <li key={item}>{item}</li>
-            //         })}
-            //         </ul>
-            //     </div>, {width: 600, height: 400, fonts: []});
-            //
+            const satoriSvg = await satori(
+                <div className="card" style={ {backgroundColor: "black", display: "flex"} }>
+                    <img alt="loot" style={ {width: "50%", float: "left"} } src={ b64svg }/>
+                    <img alt="character" style={ {width: "50%", float: "right"} } src={ img || "" }/>
+                </div>
+                , {width: 1910, height: 1000, fonts: []});
             
-            // const png = await sharp(snap)
+            const result = "data:image/svg+xml;base64," + Buffer.from(satoriSvg).toString('base64');
+            
+            await page.setViewport({width: 1910, height: 1000});
+            await page.goto(result || "about:blank", {waitUntil: "load"});
+            
+            const snap = await page.screenshot();
+            console.log("snap:", snap);
+            
+            writeFile(path, snap.toString("base64"), (err) => {
+                if (err) {
+                    console.error(err);
+                }
+                console.log("saved");
+            })
+            
+            // const png = await sharp(satoriSvg)
             //     .toFormat('png')
             //     .toBuffer();
             
