912b8d819ce52610bb35b83d7dbf72d05c371af1
Steif <40876894+Again1013@users.noreply.github.com>
2024-02-20 14:15:29 +0800

    f

--- pages/api/detail.tsx
+++ pages/api/detail.tsx
@@ -25,30 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 address = [inputArr];
             } catch (e) {
                 console.warn("Invalid address:", inputArr);
-                let buttonText = "Reveal your sLoot";
-                let imageUrl = `${ process.env['HOST'] }/1.png`;
-                
-                res.setHeader('Content-Type', 'text/html');
-                res.status(200).send(`
-                   <!DOCTYPE html>
-                   <html>
-                     <head>
-                       <title> My SLoot </title>
-                       <meta property="og:title" content="Synthetic Loot">
-                       <meta property="og:image" content="${ imageUrl }">
-                       <meta name="fc:frame" content="vNext">
-                       <meta name="fc:frame:image" content="${ imageUrl }">
-                       <meta name="fc:frame:post_url" content="${ process.env['HOST'] }/api/detail">
-                       <meta name="fc:frame:button:1" content="${ buttonText }">
-                       <meta name="fc:frame:input:text" content="Please check your input...">
-                     </head>
-                     <body>
-                          <h1> My Synthetic Loot </h1>
-                          <div style={imageUrl} >
-                          </div>
-                     </body>
-                   </html>
-                 `);
+                return res.status(400).send(`Failed to validate address: ${ e }`);
             }
             
         } else {
