424c71c33cf088a2b155e3ffc254acf03e8226e1
Steif <40876894+Again1013@users.noreply.github.com>
2024-02-20 14:20:59 +0800

    f

--- pages/api/first.tsx
+++ pages/api/first.tsx
@@ -2,10 +2,11 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
+    
     let buttonText = "Reveal your sLoot";
-    let imageUrl = `${process.env['HOST']}/1.png`;
-
+    let imageUrl = `${ process.env['HOST'] }/1.png`;
+    let inputText = "Enter address or reveal straightly...";
+    
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send(`
       <!DOCTYPE html>
@@ -13,12 +14,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         <head>
           <title> My SLoot </title>
           <meta property="og:title" content="Synthetic Loot">
-          <meta property="og:image" content="${imageUrl}">
+          <meta property="og:image" content="${ imageUrl }">
           <meta name="fc:frame" content="vNext">
-          <meta name="fc:frame:image" content="${imageUrl}">
-          <meta name="fc:frame:post_url" content="${process.env['HOST']}/api/detail">
-          <meta name="fc:frame:button:1" content="${buttonText}">
-          <meta name="fc:frame:input:text" content="Enter your address or click straightly...">
+          <meta name="fc:frame:image" content="${ imageUrl }">
+          <meta name="fc:frame:post_url" content="${ process.env['HOST'] }/api/detail">
+          <meta name="fc:frame:button:1" content="${ buttonText }">
+          <meta name="fc:frame:input:text" content="${ inputText }">
         </head>
         <body>
              <h1> My Synthetic Loot </h1>
@@ -27,5 +28,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         </body>
       </html>
     `);
-
+    
 }
