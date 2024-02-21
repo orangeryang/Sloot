1414b999d4ddb9310ca53b4ba51564648ac573ce
Steif <40876894+Again1013@users.noreply.github.com>
2024-02-21 18:38:21 +0800

    f

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Page() {
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20 text-center">
                 <div className="flex justify-center items-center bg-black rounded-full w-16 sm:w-24 h-16 sm:h-24 my-8">
-                    <VercelLogo className="h-8 sm:h-16 invert p-3 mb-1"/>
+                    {/*<VercelLogo className="h-8 sm:h-16 invert p-3 mb-1"/>*/}
                 </div>
                 <h1 className="text-lg sm:text-2xl font-bold mb-2"></h1>
                 <img src="/1-1.png" alt=""/>
