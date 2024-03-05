93180792b42a7030ae75e0d6de704a9eec619a05
Steif <40876894+Again1013@users.noreply.github.com>
2024-03-05 15:51:14 +0800

    f

--- pages/api/test/battle.tsx
+++ pages/api/test/battle.tsx
@@ -83,7 +83,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             console.log("validate result:", result);
             if (result && result.valid) {
                 user = result.action?.interactor;
-                buttonId = result.action?.button.index;
+                buttonId = result.action?.tapped_button.index;
                 opponentByInput = result.action?.input?.text || "";
             }
         } catch (e) {
