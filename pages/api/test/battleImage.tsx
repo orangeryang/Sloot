d8961de9f81ae19fd522cd626c41789154cce7bf
Steif <40876894+Again1013@users.noreply.github.com>
2024-03-05 16:26:26 +0800

    f

--- pages/api/test/battleImage.tsx
+++ pages/api/test/battleImage.tsx
@@ -6,104 +6,104 @@ import satori from "satori";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
+    
     if (req.method === 'GET') {
-
+        
         const id = req.query["id"];
         if (!id) {
             return res.status(400).send(`Failed to generate image: id not found`);
         }
         console.log("battle id:", id);
-
+        
         const prisma = new PrismaClient();
-
+        
         const battle = await prisma.battle.findUnique({
             where: {
                 id: Number.parseInt(id.toString()),
             }
         })
         console.log("find battle:", battle);
-
+        
         if (!battle) {
             console.warn("generate image: battle not found:", id);
             return res.status(400).send(`Failed to generate image: battle not found`);
         }
-
+        
         const battleDetails = await prisma.battleDetail.findMany({
             where: {
                 battleId: Number.parseInt(id.toString()),
             }
         })
         console.log("find battleDetails:", battleDetails);
-
+        
         await prisma.$disconnect();
-
+        
         const len = battleDetails.length;
         const leftAddress = req.query["address"] || battle.attacker;
         const rightAddress = battle.defender;
-
+        
         const battleLog = generateLog(battleDetails, battle);
-
+        
         const logImage = await satori(
             <div>
-                <div style={{fontFamily: "serif", fontSize: "13px", width: "600px", height: "800px", color: "white"}}>
-                    {battleLog.map((item) => (
+                <div style={ {fontFamily: "serif", fontSize: "13px", width: "600px", height: "800px", color: "white"} }>
+                    { battleLog.map((item) => (
                         <div>
-                            {item}
+                            { item }
                         </div>
-                    ))}
+                    )) }
                 </div>
             </div>,
-            {width: 1910, height: 1000, fonts: []},
+            {width: 1910, height: 1000, fonts: [{data: Buffer.from(""), name: "serif"}]},
         );
-
+        
         const leftImage = await getImageByAddress(leftAddress.toString());
         const rightImage = await getImageByAddress(rightAddress);
-
+        
         const leftCharacter = await sharp(Buffer.from(leftImage.split(",")[1], 'base64'))
             .resize(1000, 1000)
             .extract({left: 200, top: 100, width: 600, height: 750})
             // .resize({height: 1000})
             .toBuffer();
-
+        
         const rightCharacter = await sharp(Buffer.from(rightImage.split(",")[1], 'base64'))
             .resize(1000, 1000)
             .extract({left: 200, top: 100, width: 600, height: 750})
             // .resize({height: 1000})
             .toBuffer();
-
+        
         const result = await sharp(logImage)
             .composite([
                 {input: leftCharacter, left: 55, top: 100},
                 {input: rightCharacter, left: 1255, top: 100},
             ])
             .toBuffer();
-
+        
         res.setHeader('Content-Type', 'image/png');
         res.send(result);
-
+        
     } else {
         // Handle any non-POST requests
         res.setHeader('Allow', ['GET']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
+        res.status(405).end(`Method ${ req.method } Not Allowed`);
     }
-
+    
 }
 
 
 function generateLog(battleDetails: BattleDetail[], battle: Battle) {
-
+    
     let result: string[] = [];
-
+    
     if (battleDetails.length === 0) {
         return result;
     }
-
+    
     battleDetails.sort((a, b) => a.order - b.order);
     for (let i = 0; i < battleDetails.length; i += 2) {
-
+        
         const detail = battleDetails[i];
-
+        
         const isAttack = i % 2 === 0;
         const attacker = isAttack ?
             (detail.friend ? detail.friendName : battle.attackerName) :
@@ -111,7 +111,7 @@ function generateLog(battleDetails: BattleDetail[], battle: Battle) {
         const defender = isAttack ?
             battle.defenderName :
             (detail.friend ? detail.friendName : battle.attackerName);
-
+        
         // []攻击了[]，造成了[]伤害 / 暴击造成了[]伤害
         result [i] = attacker +
             " attacked " +
@@ -120,9 +120,9 @@ function generateLog(battleDetails: BattleDetail[], battle: Battle) {
             "caused " +
             detail.damage;
         console.log("generate log:", battle.id, result);
-
+        
     }
-
+    
     return result;
-
+    
 }
