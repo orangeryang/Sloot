d2ccc6b246cf7c3d76db7eceb4a8001b549c736f
Steif <40876894+Again1013@users.noreply.github.com>
2024-03-07 18:40:23 +0800

    f

--- pages/api/test/friend.tsx
+++ pages/api/test/friend.tsx
@@ -58,13 +58,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         
         let contentUrl =
             `${ process.env['HOST'] }/api/${ process.env['APIPATH'] }/battle?id=${ id }&frid=${ friendFid }&frna=${ friendName }`;
-        console.log("buttttttttttttttttt:", frna3 ? 4 : frna2 ? 3 : frna1 ? 2 : 1);
-        console.log("buttonId:", buttonId);
-        if (buttonId === frna3 ? 4 : frna2 ? 3 : frna1 ? 2 : 1) {
+        if (buttonId === (frna3 ? 4 : frna2 ? 3 : frna1 ? 2 : 1)) {
             contentUrl =
                 `${ process.env['HOST'] }/api/${ process.env['APIPATH'] }/battle?id=${ id }`;
         }
-        console.log("contentUtl:", contentUrl);
         
         res.setHeader('Content-Type', 'text/html');
         res.status(200).send(battlePage(id, imageUrl, contentUrl));
