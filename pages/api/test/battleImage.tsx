bb380502960fe85e7fef76754a35986d328fd40b
Steif <40876894+Again1013@users.noreply.github.com>
2024-03-07 16:26:20 +0800

    f

--- pages/api/test/battleImage.tsx
+++ pages/api/test/battleImage.tsx
@@ -22,8 +22,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                             font: "serif",
                             align: "centre",
                             dpi: 400
-                        }
-                    }
+                        },
+                    },
+                    left: 200,
+                    top: 450
                 }
             ]).toBuffer();
             
